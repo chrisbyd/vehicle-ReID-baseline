2e12af02ae7ff8aadffdf93cef370b1745019f2e
chrisbyd <chenyongbiao0319@sina.com>
2021-02-05 22:44:45 +0800

    no parallel

--- train_hash.py
+++ train_hash.py
@@ -70,7 +70,7 @@ def main():
     if args.load_weights and check_isfile(args.load_weights):
         load_pretrained_weights(model, args.load_weights)
 
-    model = nn.DataParallel(model).cuda() if use_gpu else model
+   # model = nn.DataParallel(model).cuda() if use_gpu else model
 
     criterion_xent = CrossEntropyLoss(num_classes=dm.num_train_pids, use_gpu=use_gpu, label_smooth=args.label_smooth)
     criterion_htri = TripletLoss(margin=args.margin)
